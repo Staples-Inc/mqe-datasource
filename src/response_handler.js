6bdc1125a2a839fe4b264ea20195e3023c731d30
Seema vk <seema.vayaliparambilkumaran@staples.com>
2016-09-07 16:43:37 -0400

    Changed the tagset - 'app' to 'cluster' and 'Hosts' to 'hosts'
    Changed the tagset - 'app' to 'cluster' and 'Hosts' to 'hosts'

--- src/response_handler.js
+++ src/response_handler.js
@@ -53,10 +53,10 @@ export function handle_explore_response(query, response) {
     return getTagset(response);
   }
   else if (query === 'cluster') {
-    return getTagset(response)['cluster'];
+    return getTagset(response)['cluster'] ;
   }
   else if (query === 'hosts') {
-    return getTagset(response)['hosts'];
+    return getTagset(response)['hosts'] ;
   }
   else {
     return response.body[query];
